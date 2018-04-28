23c56005e4f45e9cf6922d5a7d0078cd491e7928
Karen MacPherson <karen@fullstackacademy.com>
2018-04-28 09:46:42 -0400

    cycle 5

--- w2/routes/tests.js
+++ w2/routes/tests.js
@@ -1,42 +1,5 @@
 const router = require('express').Router();
 const Test = require('../db/models/test');
-const Student = require('../db/models/student');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const tests = await Test.findAll();
-    res.json(tests);
-  } catch (error) { next(error)}
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const test = await Test.findById(req.params.id);
-    res.json(test);
-  } catch (error) { next(error)}
-});
-
-router.post('/student/:studentId', async (req, res, next) => {
-  try {
-    const student = await Student.findById(req.params.studentId);
-    const test = await Test.create(req.body);
-
-    test.setStudent(student);
-    res.status(201).json(test);
-
-  } catch (error) { next(error) }
-});
-
-router.delete('/:id', async (req, res, next) => {
-  try {
-    await Test.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
-
-    res.sendStatus(204);
-  } catch (error) { next(error) }
-});
 
 module.exports = router;
