9ede1d8e9db0b266fb823b415b78e4c3b36acba0
Karen MacPherson <karen@fullstackacademy.com>
2018-04-28 09:47:51 -0400

    cycle 3

--- w2/db/models/test.js
+++ w2/db/models/test.js
@@ -1,19 +1,5 @@
 'use strict';
 const Sequelize = require('sequelize');
 const db = require('../db');
-const Student = require('./student');
-
-const Test = db.define('test', {
-  subject: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  grade: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  }
-});
-
-Test.belongsTo(Student, { as: 'student' });
 
 module.exports = Test;
