3d100f404e26c194c10feb377d7501720471d4fc
Hannah Weber <hkwweber@gmail.com>
2018-05-11 14:06:47 -0400

    add key to avoid react error

--- w3/browser/components/Main.js
+++ w3/browser/components/Main.js
@@ -1,26 +1,27 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Main extends Component {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {
             students: []
-        }
+        };
     }
 
-    componentDidMount(){
-        this.getStudents()
+    componentDidMount() {
+        this.getStudents();
     }
 
-    getStudents(){
-        console.log("fetching")
-        axios.get('/student')
-        .then(res => this.setState({students: res.data}))
-        .catch(console.error)
+    getStudents() {
+        console.log("fetching");
+        axios
+            .get("/student")
+            .then(res => this.setState({ students: res.data }))
+            .catch(console.error);
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <h1>Students</h1>
@@ -29,19 +30,16 @@ export default class Main extends Component {
                         <tr>
                             <th>Name</th>
                         </tr>
-                        {
-                            this.state.students.map(student => {
-                                return (
-                                    <tr>
-                                        <td>{student.fullName}</td>
-                                    </tr>
-                                )
-                            })
-                       }
+                        {this.state.students.map(student => {
+                            return (
+                                <tr key={student.fullName}>
+                                    <td>{student.fullName}</td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
-               
             </div>
-        )
+        );
     }
 }
